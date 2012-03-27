72ef20a4da0a22efa51ed19e5b66b0c74f23cb1b
unknown <afrancois@zaxxon.(none)>
2012-03-27 12:50:35 -0700

    Initial Commit

---
+++ src/DirectWriteApp.cpp
@@ -0,0 +1,35 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+#include <dwrite.h>
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class DirectWriteAppApp : public AppBasic {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void DirectWriteAppApp::setup()
+{
+}
+
+void DirectWriteAppApp::mouseDown( MouseEvent event )
+{
+}
+
+void DirectWriteAppApp::update()
+{
+}
+
+void DirectWriteAppApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP_BASIC( DirectWriteAppApp, RendererGl )
